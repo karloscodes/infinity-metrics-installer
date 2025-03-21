ea404a0ec1cdc91d411f85d818d40b8a6a2385cb
Carlos Castellanos <me@karloscodes.com>
2025-03-21 18:20:04 +0100

    Update Caddyfile admin address to localhost for improved security

--- internal/docker/docker.go
+++ internal/docker/docker.go
@@ -358,7 +358,7 @@ func (d *Docker) generateCaddyfile(data config.ConfigData, primaryApp, secondary
 	}
 
 	return fmt.Sprintf(`{
-    admin off
+    admin 127.0.0.1:2019
     email %s
     log {
         level INFO
